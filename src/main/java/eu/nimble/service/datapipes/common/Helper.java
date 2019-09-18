1a8f9a435f93d0e9bcc73c843d6e4b3fe930041c
a.musumeci <musumeci.holonix@gmail.com>
2019-09-18 10:56:50 +0200

    minor change for ksql compatibility

--- src/main/java/eu/nimble/service/datapipes/common/Helper.java
+++ src/main/java/eu/nimble/service/datapipes/common/Helper.java
@@ -97,7 +97,7 @@ public class Helper {
     }
 
     public static String generateInternalTopicName(String idDataChannel, String idSensor) {
-        return Configurations.INTERNAL_TOPIC_PREFIX+"-" + idDataChannel+"-" + idSensor;
+        return Configurations.INTERNAL_TOPIC_PREFIX.toUpperCase()+"_" + idDataChannel.replace('-','_')+"_" + idSensor; //changed because of https://github.com/confluentinc/ksql/issues/1466#
     }
 
     public static void updateJaasConfiguration(String username, String password) throws IOException {
